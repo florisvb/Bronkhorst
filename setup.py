152cd05abe98ee356e367897c3078cddbc345719
Floris van Breugel <floris@caltech.edu>
2015-03-16 16:08:59 -0700

    package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='Bronkhorst',
+    version='0.0.1',
+    author='CINF',
+    author_email='floris@caltech.edu',
+    packages = ['bronkhorst'],
+    license='BSD',
+    description='Python API for bronkhorst flow controllers',
+    long_description=open('README.md').read(),
+)
+
+
+
